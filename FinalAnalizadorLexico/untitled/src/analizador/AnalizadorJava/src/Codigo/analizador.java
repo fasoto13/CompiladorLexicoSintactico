88995297ded02a0db6fd34216733a8a4d90fecd1
Bryan Rodriguez <barodriguez8@utpl.edu.ec>
2022-08-04 15:46:46 -0500

    Analizador Lexico

---
+++ FinalAnalizadorLexico/untitled/src/analizador/AnalizadorJava/src/Codigo/analizador.java
@@ -0,0 +1,101 @@
+package Codigo;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import static Codigo.metodos.*;
+
+public class analizador {
+    public static void main(String[] args) throws IOException {
+        Scanner sr = new Scanner(System.in);
+        System.out.println("===============Analizador lexico=============");
+        System.out.println("             Bryan Rodriguez 1Bim            ");
+        System.out.println("---------------------------------------------");
+        int estado = 1;
+        int c = 0;
+        String text = sr.nextLine();
+        String lec_archivo = leerArchivo("C:\\Users\\ALIENWARE\\OneDrive - Universidad Técnica Particular de Loja - UTPL\\" +
+                "SISTEMAS\\6to CICLO TECNICA\\TEORIA DE AUTOMATAS Y COMPILADORES\\Trabajo_PrimerBimestre_Analizador\\" +
+                "AnalizadorJava\\src\\data\\exe.txt");
+        char[] stri = text.toCharArray();
+        if (text == "") {
+            text = "_";
+        }
+        String[] caracter = RecorrerEspaciosBn(stri, lec_archivo);
+        while (c != caracter.length) {
+            System.out.println("====================");
+            System.out.println(c);
+            System.out.println("====================");
+            switch (estado) {
+                case 1:
+                    if (buscarAritmetico(caracter[c])) {
+                        estado = 2;
+                    }
+
+                    if (buscarNumero(caracter[c])) {
+                        estado = 4;
+                    }
+
+                    if (buscarSeparador(caracter[c])) {
+                        estado = 3;
+                    }
+                    if (buscar_OpRelacional(caracter[c])) {
+                        estado = 7;
+                    }
+                    if (buscar_OpLogico(caracter[c])) {
+                        estado = 8;
+                    }
+                    if (buscarAritmetico(caracter[c]) == false && buscarSeparador(caracter[c]) == false
+                            && buscar_OpLogico(caracter[c]) == false && buscar_OpRelacional(caracter[c]) == false
+                            && buscarNumero(caracter[c]) == false) {
+                        if (buscarPReservada(caracter[c])) {
+                            estado = 5;
+                        } else {
+                            estado = 6;
+                        }
+                    }
+
+                    break;
+                case 2:
+                    System.out.println(caracter[c] + " -> es un operador aritmetico");
+                    estado = 1;
+                    c++;
+
+                    break;
+                case 3:
+                    System.out.println(caracter[c] + " -> es un separador");
+                    estado = 1;
+                    c++;
+
+                    break;
+                case 4:
+                    System.out.println(caracter[c] + " -> es un numero");
+                    estado = 1;
+                    c++;
+                    break;
+                case 5:
+                    System.out.println(caracter[c] + " -> es una palabra reservada");
+                    estado = 1;
+                    c++;
+                    break;
+                case 6:
+                    System.out.println(caracter[c] + " -> es un identificador");
+                    estado = 1;
+                    c++;
+                    break;
+                case 7:
+                    System.out.println(caracter[c] + " -> es un organizador relacional");
+                    estado = 1;
+                    c++;
+                    break;
+                case 8:
+                    System.out.println(caracter[c] + " -> es un identificador logico");
+                    estado = 1;
+                    c++;
+                    break;
+
+            }
+        }
+    }
+}
---
+++ FinalAnalizadorLexico/untitled/src/analizador/AnalizadorJava/src/Codigo/metodos.java
@@ -0,0 +1,110 @@
+package Codigo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class metodos {
+    public static boolean buscarAritmetico(String cadena) {
+        boolean estado = false;
+        String op_aritmeticos[] = {"^","$","\\","?","--"};
+        String linea ="";
+        linea = op_aritmeticos[0];
+        for (int i = 0; i < op_aritmeticos.length; i++) {
+            linea = op_aritmeticos[i];
+            if (op_aritmeticos[i].equals(cadena)) {
+                estado = true;
+            }
+        }
+        return estado;
+    }
+
+    public static boolean buscarSeparador(String cadena) {
+        boolean estado = false;
+        String separadores[] = {":", ";", "  ", ")", "_", ".", ""," ","(",",","[","]"};
+        for (int i = 0; i < separadores.length; i++) {
+            if (separadores[i].equals(cadena)) {
+                estado = true;
+            }
+        }
+        return estado;
+    }
+    public static boolean buscar_OpRelacional(String cadena) {
+        boolean estado = false;
+        String op_aritmeticos[] = {"**","hefes","zeu","!","her","apo","AA","are"};
+        for (int i = 0; i < op_aritmeticos.length; i++) {
+            if (op_aritmeticos[i].equals(cadena)) {
+                estado = true;
+            }
+        }
+        return estado;
+    }
+
+
+    public static boolean buscar_OpLogico(String cadena) {
+        boolean estado = false;
+        String op_logicos[] = {"tim","ate","kra"};
+        for (int i = 0; i < op_logicos.length; i++) {
+            if (op_logicos[i].equals(cadena)) {
+                estado = true;
+            }
+        }
+        return estado;
+    }
+
+    public static boolean buscarPReservada(String cadena) {
+        boolean estado = false;
+        String palabras_reservadas[] = {"pandora", "prometeo", "medusa", "aquiles",
+                "castor - polux", "andromeda", "switch", "bool", "main", "si", "finsi","int"
+                ,"char","inicio","fin"};
+        for (int i = 0; i < palabras_reservadas.length; i++) {
+            if (cadena.equals(palabras_reservadas[i])) {
+                estado = true;
+            }
+        }
+        return estado;
+    }
+
+    public static String[] RecorrerEspaciosBn(char[] a,String b) {
+        String palabra = "";
+        String[] espacio = b.split(" ");
+        return espacio;
+    }
+
+    public static boolean buscarNumero(String cadena){
+        boolean estado = false;
+        char[] linea = cadena.toCharArray();
+        for (int i = 0; i < cadena.length(); i++) {
+            if(Character.isDigit(linea[i])){
+                estado=true;
+            }else{
+                i=cadena.length();
+                estado=false;
+            }
+        }
+
+        return estado;
+
+    }
+    public static String leerArchivo (String archivo) throws FileNotFoundException, IOException {
+        Path path = Paths.get(archivo);
+        File doc =new File(archivo);
+        Scanner obj=new Scanner(doc);
+        String content="";
+        try {
+            content = Files.readString(path);
+            System.out.println(content);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        while (obj.hasNextLine()){
+            System.out.println(obj.nextLine());
+        }
+        return content;
+    }
+}
